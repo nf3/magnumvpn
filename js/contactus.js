a3a0408a88826a011cc63d776e7aa85bfaad15e9
nf3 <neil@neilproctor.com>
2021-04-05 13:45:55 -0600

    Move to gitpages

---
+++ js/contactus.js
@@ -0,0 +1,20 @@
+$("#contactForm").submit(function(e) {
+
+    var url = "https://stats.magnumvpn.com/isso/contactus"; // the script where you handle the form input.
+    var formdata = {};
+    $("#contactForm").serializeArray().map(function(x){formdata[x.name] = x.value;})
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: JSON.stringify(formdata), // serializes the form's elements.
+           dataType: "json",
+           contentType: "application/json; charset=utf-8",
+           success: function(data)
+           {
+               alert("Thanks for the comment!"); // show response from the php script.
+               $('#contactForm').trigger("reset");
+           }
+         });
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+});
